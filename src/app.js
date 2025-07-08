0f27a8e64deb73b5eb8a39e9280420b4e58b43a1
MidnytDeveloper <abhishek.sni101@gmail.com>
2025-07-08 22:44:36 +0530

    dynamic data api

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
+const User = require("./models/User.js");
+
+app.use(express.json());
+
+app.post("/signup", async (req, res) => {
+  const user = new User(req.body);
+  try {
+    await user.save();
+    res.send("User Added Successfully");
+  } catch (err) {
+    res.status(400).send("Error saving the user : " + err.message);
+  }
+});
 
 connectDB()
   .then(() => {
     console.log("Database connection established");
-
     app.listen(3000, () => {
       console.log("server is created successfully and listen on port 3000");
     });
